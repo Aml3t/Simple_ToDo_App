085969d387c785a945b0d6a0253863048220e5f3
Aml3t <fischerparsos@gmail.com>
2023-10-06 12:16:26 +0300

    Class configuration

--- Globomatics.Domain/TodoTask.cs
+++ Globomatics.Domain/TodoTask.cs
@@ -12,23 +12,4 @@ namespace Globomantics.Domain
         User CreatedBy)
         : Todo(Guid.NewGuid(), Title, DateTimeOffset.UtcNow, CreatedBy);
 
-
-    public record Feature(string Title,
-        string Description,
-        string Component,
-        int Priority,
-        User CreatedBy,
-        User AssignedTo) : TodoTask(Title, DateTimeOffset.MinValue, CreatedBy);
-
-
-    public record Bug(string Title,
-        string Description,
-        Severity Severity,
-        string AffectedVersion,
-        int AffectedUsers,
-        User CreatedBy,
-        User? AssignedTo,
-        IEnumerable<byte[]> Images) : TodoTask(Title, DateTimeOffset.MinValue, CreatedBy);
-
-
 }
