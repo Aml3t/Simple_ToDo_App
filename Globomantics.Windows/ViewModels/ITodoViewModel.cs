2483951e770a5b58adb2f152900c5681794d35e5
Aml3t <fischerparsos@gmail.com>
2023-10-06 15:19:59 +0300

    Setting classes for ViewModels

--- Globomantics.Windows/ViewModels/ITodoViewModel.cs
+++ Globomantics.Windows/ViewModels/ITodoViewModel.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Globomantics.Windows.ViewModels
 {
     public interface ITodoViewModel : IViewModel
     {
         IEnumerable<Todo> AvailableParentTasks { get; set; }
+        ICommand DeleteCommand { get; }
+        ICommand SaveCommand { get; }
+        Task SaveAsync();
+        void UpdateMode(Todo model);
+
+
+
+
     }
 }
