70d61f65a8cfb6becee1c6a4ed4d0f6d90db4142
Aml3t <fischerparsos@gmail.com>
2023-10-06 14:48:37 +0300

    Setting interfaces for ViewModels

---
+++ Globomantics.Windows/ViewModels/ITodoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Globomantics.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Windows.ViewModels
+{
+    public interface ITodoViewModel : IViewModel
+    {
+        IEnumerable<Todo> AvailableParentTasks { get; set; }
+    }
+}
