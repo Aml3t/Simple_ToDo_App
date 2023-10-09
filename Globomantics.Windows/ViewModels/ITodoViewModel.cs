8b2d9585ff171c04ad5250810d3b6f10dd94b2b9
Aml3t <fischerparsos@gmail.com>
2023-10-09 12:10:03 +0300

    Uploading some corrupted files

---
+++ Globomantics.Windows/ViewModels/ITodoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Globomantics.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Windows.ViewModels
+{
+    public interface ITodoViewModel : IViewModel
+    {
+        IEnumerable<Todo>? AvailableParentTasks { get; set; }
+
+
+    }
+}
