c23eb13f2cf447e24badf89579a651a733eec662
Aml3t <fischerparsos@gmail.com>
2023-10-09 12:28:23 +0300

    Setting various ViewModels

---
+++ Globomantics.Windows/ViewModels/BaseTodoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Globomantics.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Windows.ViewModels
+{
+    public abstract class BaseTodoViewModel<T> : ObservableObject where T : Todo
+    {
+    }
+}
--- Globomantics.Windows/ViewModels/ITodoViewModel.cs
+++ Globomantics.Windows/ViewModels/ITodoViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Globomantics.Windows.ViewModels
 {
@@ -11,6 +12,9 @@ namespace Globomantics.Windows.ViewModels
     {
         IEnumerable<Todo>? AvailableParentTasks { get; set; }
 
-
+        ICommand DeleteCommand { get; }
+        ICommand SaveCommand { get; }
+        Task SaveAsync();
+        public void UpdateModel(Todo model);
     }
 }
