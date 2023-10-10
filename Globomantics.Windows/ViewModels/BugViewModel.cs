729bddc65d60d7f287e2998a86be6301f9b73bed
Aml3t <fischerparsos@gmail.com>
2023-10-10 13:29:59 +0300

    Setting Bug ViewModel

---
+++ Globomantics.Windows/ViewModels/BugViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Globomantics.Domain;
+using Globomantics.Infrastructure.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Windows.ViewModels
+{
+    public class BugViewModel : BaseTodoViewModel<Bug>
+    {
+        private readonly IRepository<Feature> repository;
+
+        private string? description;
+        public string? Description
+        {
+            get => description;
+
+            set
+            {
+                description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public BugViewModel(IRepository<Feature> repository) : base()
+        {
+            this.repository = repository;
+        }
+
+        public override Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
