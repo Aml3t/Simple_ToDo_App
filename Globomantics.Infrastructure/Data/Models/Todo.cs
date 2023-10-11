a825bb4bea55d2470ffa8a5bee11c97286a37a73
Aml3t <fischerparsos@gmail.com>
2023-10-11 16:01:39 +0300

    Adding Models for the Data layer

--- Globomantics.Infrastructure/Data/Models/Todo.cs
+++ Globomantics.Infrastructure/Data/Models/Todo.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Globomantics.Domain;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,11 @@ namespace Globomantics.Infrastructure.Data.Models
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Title { get; set; } = default!;
         public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.UtcNow;
+        public User CreatedBy { get; set; } = default!;
+        public bool IsCompleted { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual Todo? Parent { get; set; }
+
 
     }
 }
