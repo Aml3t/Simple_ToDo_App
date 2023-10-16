6a93dc4fd0629bb30e850d6830c9a374ded426ab
Aml3t <fischerparsos@gmail.com>
2023-10-16 13:52:31 +0300

    Mapping domain to data

--- Globomantics.Infrastructure/Data/DomainToDataMapping.cs
+++ Globomantics.Infrastructure/Data/DomainToDataMapping.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Globomantics.Infrastructure.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -13,8 +14,29 @@ namespace Globomantics.Infrastructure.Data
             where TFrom : Domain.Todo
             where TTo : Data.Models.Todo
         {
+            var model = input switch
+            {
+                Data.Models.Bug bug => MapBug(bug),
+                Data.Models.Feature feature => MapFeature(feature),
+                Data.Models.TodoTask task => MapTask(task),
+            }
 
+            return model;
+        }
 
+        private static object MapTask(TodoTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object MapFeature(Feature feature)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object MapBug(Bug bug)
+        {
+            throw new NotImplementedException();
         }
     }
 }
