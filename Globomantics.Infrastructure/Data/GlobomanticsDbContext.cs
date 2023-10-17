4f9aa476f1dc0c76c5bc22e7a83167ecbf35a7ba
Aml3t <fischerparsos@gmail.com>
2023-10-17 11:07:15 +0300

    Mapping domain to data

--- Globomantics.Infrastructure/Data/GlobomanticsDbContext.cs
+++ Globomantics.Infrastructure/Data/GlobomanticsDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Globomantics.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Globomantics.Infrastructure.Data
 {
-    internal class GlobomanticsDbContext
+    public class GlobomanticsDbContext : DbContext
     {
+        public DbSet<TodoTask> TodoTasks { get; set; }
+        public DbSet<Bug> Bugs { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Image> Images { get; set; }
     }
 }
