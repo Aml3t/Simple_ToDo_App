fd9d254c781d19236cc32722226ff38fdf573a65
Aml3t <fischerparsos@gmail.com>
2023-10-19 15:17:33 +0300

    Creating additional repositories

---
+++ Globomantics.Infrastructure/Data/Repositories/FeatureRepository.cs
@@ -0,0 +1,27 @@
+﻿using Globomantics.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Infrastructure.Data.Repositories
+{
+    public class FeatureRepository : TodoRepository<Feature>
+    {
+        public FeatureRepository(GlobomanticsDbContext context) : base(context)
+        {
+
+        }
+
+        public override Task AddAsync(Feature item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Feature> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
